2d6546d995e7e7f3de5f35971ccfa2ec2ba700ff
Pooria Azami <pooriaazami@gmail.com>
2023-11-12 22:04:35 +0330

    environemtn created

---
+++ src/Env.py
@@ -0,0 +1,60 @@
+import numpy as np
+from PIL import Image   
+from matplotlib import cm
+
+import gymnasium as gym
+
+
+class Environment:
+
+    IMG_SIZE = 84
+
+    def __init__(self):
+        self.__env = gym.make("Breakout-v4", render_mode="human")
+
+    def __preprocess(self, observation):
+        img = np.dot(observation[...,:3], [0.2989, 0.5870, 0.1140])
+
+        img = Image.fromarray(np.uint8(cm.gist_earth(img)*255))
+        img = img.resize((110, 84))
+
+        width, height = img.size
+        new_width, new_height = Environment.IMG_SIZE, Environment.IMG_SIZE
+
+        left = (width - new_width) / 2
+        top = (height - new_height) / 2
+        2
+        right = (width + new_width) / 2
+        bottom = (height + new_height) / 2
+
+        img = img.crop((left, top, right, bottom))
+
+        return np.array(img)
+
+    def step(self, action):
+        observations, rewards, ended = [], 0, False
+
+        for _ in range(4):
+            if ended:
+                observation = np.zeros((210, 160, 3))
+                reward = 0
+                terminated, truncated = True, True
+            else:
+                observation, reward, terminated, truncated, _ = self.__env.step(action)
+
+            observation = self.__preprocess(observation)
+            observations.append(observation)
+
+            rewards += reward
+
+            if ended or terminated or truncated:
+                ended = True
+
+        return np.array(observations), rewards, ended
+    
+
+    def reset(self):
+        self.__env.reset()
+
+        
+        
