3812f98155227e7c0bcfe5a0cd5448a3331bb335
Pooria Azami <pooriaazami@gmail.com>
2023-11-21 20:48:11 +0330

    flag removed + input image transpose

--- src/Memory.py
+++ src/Memory.py
@@ -1,14 +1,17 @@
 import random
 from collections import namedtuple, deque
 
-MemoryElement = namedtuple('MemoryElement', ['current_state', 'reward', 'action', 'next_state', 'terminal'])
+MemoryElement = namedtuple('MemoryElement', ['current_state', 'reward', 'action', 'next_state'])
 
 class Memory:
     def __init__(self, maxlen=1000):
         self.__buffer = deque(maxlen=maxlen)
 
-    def push(self, current_state, reward, action, next_state, terminal):
-        memory_element = MemoryElement(current_state, reward, action, next_state, terminal)
+    def push(self, current_state, reward, action, next_state):
+        current_state = current_state.transpose((2, 0, 1))
+        next_state = current_state.transpose((2, 0, 1))
+        
+        memory_element = MemoryElement(current_state, reward, action, next_state)
         self.__buffer.append(memory_element)
 
     def sample(self, batch_size):
